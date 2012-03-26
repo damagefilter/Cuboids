abc6495885e6bb1eea9427df4edda1a42709057d
Glacksy <quazgaming@gmail.com>
2012-03-26 22:12:13 +0300

    Update src/net/playblack/datasource/MysqlData.java

--- src/net/playblack/datasource/MysqlData.java
+++ src/net/playblack/datasource/MysqlData.java
@@ -58,7 +58,7 @@ public class MysqlData extends BaseData {
 						"`welcome` varchar(120) DEFAULT NULL," +
 						"`wheatControl` varchar(5) DEFAULT NULL," +
 						"`world` varchar(45) DEFAULT NULL," +
-						"`hmob` varchar(5) DEFAULT NULL, " +
+						"`hmob` varchar(5) DEFAULT NULL," +
 						"PRIMARY KEY (`id`)" +
 						") ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1");
 				ps.execute();
